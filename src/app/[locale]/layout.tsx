14b80726c48adb6210e671c1de94533f73324b88
MrYapikZ <yafialhaidar18@gmail.com>
2025-10-20 11:48:30 +0700

    feat(ui): setup i18n

---
+++ src/app/[locale]/layout.tsx
@@ -0,0 +1,29 @@
+import {NextIntlClientProvider, hasLocale} from 'next-intl';
+import {notFound} from 'next/navigation';
+import {routing} from '@/i18n/routing';
+import React from "react";
+
+type Props = {
+    children: React.ReactNode;
+    params: Promise<{ locale: string }>;
+};
+
+export default async function LocaleLayout({children, params}: Props) {
+    // Ensure that the incoming `locale` is valid
+    const {locale} = await params;
+    if (!hasLocale(routing.locales, locale)) {
+        notFound();
+    }
+
+    return (
+        <html lang={locale} suppressHydrationWarning>
+        <body
+            className={`antialiased`}
+        >
+        <NextIntlClientProvider>
+            {children}
+        </NextIntlClientProvider>
+        </body>
+        </html>
+    );
+}
