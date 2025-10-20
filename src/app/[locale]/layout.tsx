a0c22609f28056ad2d413486c80757f6bc8cc747
MrYapikZ <yafialhaidar18@gmail.com>
2025-10-20 11:59:16 +0700

    refactor: styles forlder structure

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -2,6 +2,7 @@ import {NextIntlClientProvider, hasLocale} from 'next-intl';
 import {notFound} from 'next/navigation';
 import {routing} from '@/i18n/routing';
 import React from "react";
+import '@/app/styles/globals.css';
 
 type Props = {
     children: React.ReactNode;
