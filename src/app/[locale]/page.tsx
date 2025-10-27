f2cd6eabf913fa2098fb8cbe013f074f8532934c
MrYapikZ <yafialhaidar18@gmail.com>
2025-10-27 09:59:04 +0700

    fix(ui): clean up root page

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,11 +1,8 @@
-import {useTranslations} from 'next-intl';
+import React from 'react';
 import HeroBackground from "@/components/home/HeroBackground";
-import CursorEffect from "@/components/CursorEffect";
 
 export default function HomePage() {
-    const t = useTranslations('HomePage');
     return <>
-        {/*<h1>{t('title')}</h1>*/}
         <HeroBackground/>
     </>;
 }
