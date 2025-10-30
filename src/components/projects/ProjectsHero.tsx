22eb27715f0136c0a134d2165f20fb55084f03e5
MrYapikZ <yafialhaidar18@gmail.com>
2025-10-30 14:38:23 +0700

    fix(projects): implement i18n to hero

--- src/components/projects/ProjectsHero.tsx
+++ src/components/projects/ProjectsHero.tsx
@@ -1,12 +1,16 @@
+'use client';
 import React from 'react';
+import {useTranslations} from "use-intl";
 
 function ProjectsHero() {
+    const t = useTranslations();
+
     return (
         <>
             <div className="relative w-full h-screen">
                 <div className="w-full h-full flex justify-center items-end">
                     <h1 className="text-[clamp(6rem,20vw,55rem)] font-bebas-neue leading-none">
-                        Our Projects
+                        {t('Projects.title')}
                     </h1>
                 </div>
             </div>
