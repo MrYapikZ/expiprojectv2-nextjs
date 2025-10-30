678486f2780f3facf491094c0b3ffe3cda428c0f
MrYapikZ <yafialhaidar18@gmail.com>
2025-10-30 14:00:48 +0700

    feat(projects): basic hero

---
+++ src/app/[locale]/projects/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ProjectsHero from "@/components/projects/ProjectsHero";
+
+export default function ProjectsPage() {
+    return (
+        <>
+            <ProjectsHero/>
+        </>
+    );
+}
---
+++ src/components/projects/ProjectsHero.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function ProjectsHero() {
+    return (
+        <>
+            <div className="relative w-full h-screen">
+                <div className="w-full h-full flex justify-center items-end">
+                    <h1 className="text-[clamp(6rem,20vw,55rem)] font-bebas-neue leading-none">
+                        Our Projects
+                    </h1>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default ProjectsHero;
